0323fdfce2f13f4da8030fab9c617969ae7df064
Grégory Bataille <gregory.bataille@gmail.com>
2018-09-08 13:19:35 +0200

    feat(): output formats

---
+++ src/EnvStatus/Output/Types.hs
@@ -0,0 +1,11 @@
+module EnvStatus.Output.Types where
+
+data OutputFormat = ZSH | BASH | TMUX | NONE | Other deriving (Show)
+instance Read OutputFormat where
+  readsPrec _ input =
+    case input of
+        "zsh" -> return (ZSH, "")
+        "bash" -> return (BASH, "")
+        "tmux" -> return (TMUX, "")
+        "none" -> return (NONE, "")
+        _ -> return (Other, "")
---
+++ src/Main.hs
@@ -0,0 +1,44 @@
+module Main where
+
+import Control.Monad (unless)
+import System.Environment (getArgs)
+import System.Exit (exitSuccess)
+
+import EnvStatus.Output.Types
+
+main :: IO ()
+main = do
+    args <- getArgs
+    validateArgs args
+    let shell = (read . head $ args)::OutputFormat
+    print shell
+    putStrLn "done"
+
+validateArgs :: [String] -> IO ()
+validateArgs args =
+  unless (areArgsValid args) $ do
+    printHelp
+    exitSuccess
+
+areArgsValid :: [String] -> Bool
+areArgsValid args
+  | null args = True
+  | length args > 1 = False
+  | head args == "--help" = False
+  | otherwise = True
+
+printHelp :: IO ()
+printHelp = putStrLn $
+  unlines [ ""
+          , "envstatus [OPTIONS]"
+          , ""
+          , "envstatus extracts numerous information from the current execution environment and outputs it"
+          , ""
+          , "OPTIONS:"
+          , "  --help            Displays this help"
+          , ""
+          , "  zsh               Outputs the environment details, using zsh PROMPT escape characters"
+          , "  bash              Outputs the environment details, using bash PROMPT escape characters"
+          , "  tmux              Outputs the environment details, using tmux PROMPT escape characters"
+          , "  none              Outputs the environment details, using no escape characters (no color support)"
+          ]
