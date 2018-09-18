239beb66af5c25508daebd43ada5ed5b64115bfc
Grégory Bataille <gregory.bataille@gmail.com>
2018-09-18 18:34:36 +0200

    test(): move to Hspec

--- test/Test/EnvStatus/Config.hs
+++ test/Test/EnvStatus/Config.hs
@@ -4,9 +4,10 @@ module Test.EnvStatus.Config (configTests) where
 import Data.ConfigFile (emptyCP, get, to_string)
 import Data.Either
 import PyF
+import System.IO.Unsafe
 
 import Test.Tasty
-import Test.Tasty.HUnit
+import Test.Tasty.Hspec
 
 import EnvStatus.Config
 
@@ -17,26 +18,22 @@ configTests = testGroup "Config Tests"
   ]
 
 testReadConfig :: TestTree
-testReadConfig = testGroup "#readConfig"
-  [ testReadConfigErrorsOnBadFormat
-    , testReadConfigProduceProperConfigParser
-  ]
+testReadConfig = unsafePerformIO $
+  testSpec "Config Tests" $ do
+    describe "#readConfig" $ do
+      it "returns an empty config when there is a parser error" $ do
+        cp <- readConfig "[Section]text"
+        to_string cp `shouldBe` to_string emptyCP
 
-testReadConfigErrorsOnBadFormat :: TestTree
-testReadConfigErrorsOnBadFormat = testCase "errors on bad format: section + text on same line" $ do
-  cp <- readConfig "[Section]text"
-  to_string cp @?= to_string emptyCP
+      it "returns a proper config object" $ do
+        let k1 = "foo"
+        let v1 = "bar"
+        let k2 = "toto"
+        let v2 = "tutu"
+        let section = "section"
+        let conf = [fString|{k1}: {v1}\n[{section}]\n{k2}: {v2}|]
 
-testReadConfigProduceProperConfigParser :: TestTree
-testReadConfigProduceProperConfigParser = testCase "produces proper ConfigParser object" $ do
-  let k1 = "foo"
-  let v1 = "bar"
-  let k2 = "toto"
-  let v2 = "tutu"
-  let section = "section"
-  let conf = [fString|{k1}: {v1}\n[{section}]\n{k2}: {v2}|]
-
-  cp <- readConfig conf
-  assertBool "ConfigParser should not be empty" $ to_string emptyCP /= to_string cp
-  fromRight "" (get cp "DEFAULT" k1) @?= v1
-  fromRight "" (get cp section k2) @?= v2
+        cp <- readConfig conf
+        to_string emptyCP `shouldSatisfy` (to_string cp /=)
+        fromRight "" (get cp "DEFAULT" k1) `shouldBe` v1
+        fromRight "" (get cp section k2) `shouldBe` v2
